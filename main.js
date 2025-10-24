a2e7da073116a1c2861fa988d3e14b5368a957cb
Ogo Nelson <goldnelsgroup@gmail.com>
2025-10-24 18:26:08 +0100

    feat: added the route for each feature

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ const app = express();
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, "public")));
-//app.use("/api/v1/games", gameRouter);
+app.use("/api/v1/games", gameRouter);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "public", "index.html"));
