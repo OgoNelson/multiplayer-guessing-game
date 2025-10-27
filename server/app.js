3f894aba70fb33e607c842df568f4cce2efe772b
Ogo Nelson <goldnelsgroup@gmail.com>
2025-10-27 06:42:57 +0100

    fix: fixed the bug with deployment

--- server/app.js
+++ server/app.js
@@ -12,7 +12,9 @@ const io = socketIo(server, {
       "http://localhost:5174",
       "http://localhost:5173",
       "http://localhost:3000",
-    ],
+      process.env.RAILWAY_PUBLIC_DOMAIN ? `https://${process.env.RAILWAY_PUBLIC_DOMAIN}` : "",
+      process.env.RAILWAY_STATIC_URL ? process.env.RAILWAY_STATIC_URL : "",
+    ].filter(Boolean),
     methods: ["GET", "POST"],
   },
 });
